b9965ba880cda81389b2d89c028df0a56ca3dd4f
shreyas <shreyasnaik-01>
2023-12-03 23:50:57 +0530

    |#suntek|feeback_changes_on_field_validation|

--- suntek_app/suntek/doctype/site_survey/site_survey.js
+++ suntek_app/suntek/doctype/site_survey/site_survey.js
@@ -1,11 +1,13 @@
-frappe.ui.form.on('Site Survey', {
+frappe.ui.form.on("Site Survey", {
     refresh: function(frm) {
+        console.log("innnnnnnnnnnnnnnnnnnnnn")
 
         frm.add_custom_button(__('Designing'), function() {
     
             frappe.model.with_doctype('Designing', function() {
                 var DesigningDoc = frappe.model.get_new_doc('Designing');
-				
+                console.log(frm.doc.site_engineer)
+				DesigningDoc.designer_name = frm.doc.site_engineer
 				DesigningDoc.site_survey = frm.doc.name;
                 DesigningDoc.opportunity_name = frm.doc.opportunity_name; 
                 
@@ -15,5 +17,22 @@ frappe.ui.form.on('Site Survey', {
 
             });
         }, __('Create'));
-    }
+        frm.fields_dict['site_engineer'].get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    'department': frm.doc.department
+                }
+            };
+        };
+        frm.fields_dict['department'].df.onchange = function() {
+            frm.set_value('site_engineer', '');
+            frm.refresh_field('site_engineer');
+        };
+    },
+   
+        // Add a filter to the Site Engineer field based on the Department field
+       
+
 });
+
+
