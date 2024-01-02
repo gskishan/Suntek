996ad4145de172de057678d7885a4ac3a7e859e0
kishan <kishan123@gmail.com>
2024-01-02 14:54:48 +0530

    |#suntek|product_bundle_changes|

--- suntek_app/suntek/custom/product_bundle.py
+++ suntek_app/suntek/custom/product_bundle.py
@@ -1,22 +1,24 @@
-# import frappe
+from operator import itemgetter
+import frappe
 
-# @frappe.whitelist()
-# def update_product_bundle_rate_price(docname):
-#     product_bundle_doc = frappe.get_doc("Product Bundle", docname)
-
-#     for item in product_bundle_doc.items:
-#         item_price_rate = frappe.db.get_value("Item Price", {"item_code": item.item_code}, "price_list_rate")
-         
-#         if item_price_rate is not None:
-        
-#             calculated_amount = item.qty * item_price_rate
-
-#             frappe.db.set_value("Product Bundle Item", item.name, "rate", item_price_rate)
-#             frappe.db.set_value("Product Bundle Item", item.name, "custom_amount", calculated_amount)
-#         else:
-#             frappe.db.set_value("Product Bundle Item", item.name, "rate", 0)
-#             frappe.db.set_value("Product Bundle Item", item.name, "custom_amount", 0)
+@frappe.whitelist()
+def update_product_bundle_rate_price(item_code):
+    selling_rate = []
+    buying_rate = []
+    if item_code:
+        item_price_rate_selling = frappe.db.get_value("Item Price", {"item_code":item_code,"price_list":"Standard Selling"},["price_list_rate"])
     
-#     return item
+        item_price_rate_buying = frappe.db.get_value("Item Price", {"item_code":item_code,"price_list":"Standard Buying"},["price_list_rate"])
+
+    
+    if item_price_rate_selling:
+        selling_rate.append(item_price_rate_selling)
+    if item_price_rate_buying:
+        buying_rate.append(item_price_rate_buying)
+   
+    return selling_rate,buying_rate 
+
+    
+  
 
    
