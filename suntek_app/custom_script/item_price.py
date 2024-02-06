6f93f2fb480a11f1c4af262e94f1781071ab9b14
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-06 21:09:07 +0530

    Update item_price.py

--- suntek_app/custom_script/item_price.py
+++ suntek_app/custom_script/item_price.py
@@ -5,3 +5,4 @@ def validate(self, method):
     if self.percentage != 0:
         change_rate = self.price_list_rate + (self.price_list_rate * (self.percentage / 100))
         self.db_set("price_list_rate", change_rate)
+        self.db_set("percentage",0)
