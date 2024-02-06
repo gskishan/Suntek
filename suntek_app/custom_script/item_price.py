1f726a365bd2dcd054a86bb62b4d724ae3da21ff
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-06 21:02:02 +0530

    Update item_price.py

--- suntek_app/custom_script/item_price.py
+++ suntek_app/custom_script/item_price.py
@@ -1,7 +1,7 @@
 import frappe
+
 @frappe.whitelist()
-def validate(self,method):
-        frappe.errprint("working")
-        if self.percentage !=0:
-            change_rate=self.price_list_rate+ ( self.price_list_rate * self.price_list_rate)
-            self.db_set("price_list_rate",change_rate)
+def validate(self, method):
+    if self.percentage != 0:
+        change_rate = self.price_list_rate + (self.price_list_rate * (self.percentage / 100))
+        self.db_set("price_list_rate", change_rate)
