38754addc941a5e7bad27f9fabb4fbff8abb60d4
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-08 17:28:32 +0530

    Update item_price.py

--- suntek_app/custom_script/item_price.py
+++ suntek_app/custom_script/item_price.py
@@ -6,7 +6,7 @@ def validate(self, method):
     #     change_rate = self.price_list_rate + (self.price_list_rate * (self.percentage / 100))
     #     self.db_set("price_list_rate", change_rate)
     if self.is_new() and self.price_list=="Standard Buying":
-        ss=frappe.get_doc("Price List")
+        ss=frappe.get_doc("Price List","Standard Selling")
         new=frappe.new_doc("Item Price")
         new.price_list="Standard Selling"
         new.item_code=self.item_code
