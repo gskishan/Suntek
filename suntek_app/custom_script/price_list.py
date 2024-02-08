9be96c4e3801b0a3341fa2d4e591261a4e2e2b2f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-08 13:54:20 +0530

    Update price_list.py

--- suntek_app/custom_script/price_list.py
+++ suntek_app/custom_script/price_list.py
@@ -2,9 +2,11 @@ import frappe
 
 @frappe.whitelist()
 def validate(self, method):
+	frappe.errprint("working")
 	if self.percentage != 0:
 		sql="""select * from `tabItem Price` where price_list="{0}" """.format(self.name)
 		data=frappe.db.sql(sql,as_dict=True)
+		frappe.errprint(data)
 		if len(data)>100:
 			frappe.enqueue(update_rate, queue='long', param1=self, param2=data)
 		else:
@@ -13,6 +15,7 @@ def validate(self, method):
 
 
 def update_rate(self,data):
+	frappe.errprint([len(data),"da])
 	for d in data:
 		doc=frappe.get_doc("Item Price",d.name)
 		change_rate = doc.price_list_rate + (doc.price_list_rate * (self.percentage / 100))
