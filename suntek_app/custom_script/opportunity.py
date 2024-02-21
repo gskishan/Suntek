79260300d05852f214940e63b23eafa1ba1a6701
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-21 15:09:26 +0530

    Update opportunity.py

--- suntek_app/custom_script/opportunity.py
+++ suntek_app/custom_script/opportunity.py
@@ -1,5 +1,8 @@
 import frappe
 @frappe.whitelist()
 def get_emp(user):
-  sql="""select * from `tabEmployee` e inner join `tabSales Person` s on e.name=s.employee where user_id="{0}" """.format(user)
-  return(frappe.db.sql(sql,as_dict=True))
+  sql="""select e.cell_number,s.name from `tabEmployee` e inner join `tabSales Person` s on e.name=s.employee where user_id="{0}" """.format(user)
+  data=(frappe.db.sql(sql,as_dict=True))
+  if data:
+    return data
+    
