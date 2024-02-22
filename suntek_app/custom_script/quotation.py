c5f612670a10c2f746bdfd1d52cc410e78c55e4b
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-22 20:11:02 +0530

    Update quotation.py

--- suntek_app/custom_script/quotation.py
+++ suntek_app/custom_script/quotation.py
@@ -1,6 +1,7 @@
 import frappe
 @frappe.whitelist()
 def validate(self,method):
-    for d in self.items:
-        d.custom_total_gst_rate=d.igst_rate+d.cgst_rate+d.sgst_rate
-        d.custom_total_gst_amount=d.igst_amount+d.cgst_amount+d.sgst_amount
+    pass
+    #for d in self.items:
+        #d.custom_total_gst_rate=d.igst_rate+d.cgst_rate+d.sgst_rate
+        #d.custom_total_gst_amount=d.igst_amount+d.cgst_amount+d.sgst_amount
