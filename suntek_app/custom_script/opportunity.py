d55070d8301c0c48156066c9b04f21e5d39de9f6
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-29 19:35:45 +0530

    Update opportunity.py

--- suntek_app/custom_script/opportunity.py
+++ suntek_app/custom_script/opportunity.py
@@ -5,4 +5,30 @@ def get_emp(user):
   data=(frappe.db.sql(sql,as_dict=True))
   if data:
     return data
+
+@frappe.whitelist()
+def on_update(self,method):
+  from frappe.utils import nowdate
+  if self.opportunity_owner:
+    filters = {
+			"reference_type": self.doctype,
+			"reference_name": self.name,
+			"status": "Open",
+			"allocated_to": self.opportunity_owner,
+		}
+    if not frappe.get_all("ToDo", filters=filters):
+      d = frappe.get_doc(
+				{
+					"doctype": "ToDo",
+					"allocated_to": self.opportunity_owner,
+					"reference_type": self.doctype,
+					"reference_name": self.name,
+					"description":self.customer_name,
+					"priority": "Medium",
+					"status": "Open",
+					"date": nowdate(),
+					"assigned_by": frappe.session.user,
+					"assignment_rule": "",
+				}
+			).insert(ignore_permissions=True)
     
