cd962bbfef443c44fd6e140bd5b997e7317d4687
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-02 14:20:16 +0530

    Update opportunity.py

--- suntek_app/custom_script/opportunity.py
+++ suntek_app/custom_script/opportunity.py
@@ -19,21 +19,21 @@ def on_update(self,method):
 				"status": "Open",
 				"allocated_to": data[0].user_id,
 				}
-		if not frappe.get_all("ToDo", filters=filters):
-			d = frappe.get_doc(
-						{
-							"doctype": "ToDo",
-							"allocated_to": data[0].user_id,
-							"reference_type": self.doctype,
-							"reference_name": self.name,
-							"description":self.customer_name,
-							"priority": "Medium",
-							"status": "Open",
-							"date": nowdate(),
-							"assigned_by": frappe.session.user,
-							"assignment_rule": "",
-						}
-					).insert(ignore_permissions=True)
+			if not frappe.get_all("ToDo", filters=filters):
+				d = frappe.get_doc(
+                            {
+                                "doctype": "ToDo",
+                                "allocated_to": data[0].user_id,
+                                "reference_type": self.doctype,
+                                "reference_name": self.name,
+                                "description":self.customer_name,
+                                "priority": "Medium",
+                                "status": "Open",
+                                "date": nowdate(),
+                                "assigned_by": frappe.session.user,
+                                "assignment_rule": "",
+                            }
+                        ).insert(ignore_permissions=True)
 	  
 def get_salesman_user(self):
 	sql="""select  e.name , user_id from `tabSales Person` s inner join `tabEmployee` e on s.employee=e.name where s.employee 
