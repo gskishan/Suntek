5148d161d6971f9d5c9afae66bf7d811d990d877
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-12 11:16:38 +0530

    Update lead.py

--- suntek_app/suntek/custom/lead.py
+++ suntek_app/suntek/custom/lead.py
@@ -4,8 +4,21 @@ def change_enquiry_status(doc,method):
     
     if doc.custom_enquiry_status:
         doc.status = doc.custom_enquiry_status
+    duplicate_check(doc)
 
 def set_enquiry_name(doc,method):
 
     if doc.name:
         doc.custom_enquiry_name = doc.name
+        
+def duplicate_check(doc):
+    leads = frappe.db.get_list('Lead',
+    filters={
+        'mobile_no': doc.mobile_no
+    },
+    fields=['name', 'mobile_no'],
+    as_list=True
+    )
+
+    if leads:
+        frappe.throw("Duplicate Mobile no {0}".format(doc.mobile_no))
