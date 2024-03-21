0fdd6a5602bcce6d1b3753da75748b5bd28ebe56
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-21 12:45:08 +0530

    Update lead.py

--- suntek_app/custom_script/lead.py
+++ suntek_app/custom_script/lead.py
@@ -3,11 +3,11 @@ import frappe
 def validate(doc,method):
     leads = frappe.db.get_list('Lead',
     filters={
-        'mobile_no': doc.mobile_no
+        'mobile_no': doc.mobile_no,
+        'name': ('!=', doc.name)  
     },
     fields=['name', 'mobile_no'],
     as_list=True
     )
-
     if leads:
         frappe.throw("Duplicate Mobile no {0}".format(doc.mobile_no))
