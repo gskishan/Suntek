0b8f16eac29dbb147a1b6b49cec979d3b1f17683
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-21 19:23:48 +0530

    Update opportunity.py

--- suntek_app/suntek/custom/opportunity.py
+++ suntek_app/suntek/custom/opportunity.py
@@ -8,3 +8,27 @@ def set_opportunity_name(doc,method):
 
     if doc.name:
         doc.custom_opportunity_name = doc.name
+
+
+@frappe.whitelist()
+def custom_make_customer(source_name, target_doc=None):
+	def set_missing_values(source, target):
+		target.opportunity_name = source.name
+
+		if source.opportunity_from == "Lead":
+			target.lead_name = source.party_name
+
+	doclist = get_mapped_doc(
+		"Opportunity",
+		source_name,
+		{
+			"Opportunity": {
+				"doctype": "Customer",
+				"field_map": {"currency": "default_currency", "customer_name": "customer_name"},
+			}
+		},
+		target_doc,
+		set_missing_values,
+	)
+
+	return doclist
