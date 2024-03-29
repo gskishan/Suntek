7021557ea8f77241b598cc9e3ee087a974ef176c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-29 11:20:43 +0530

    Update opportunity.py

--- suntek_app/suntek/custom/opportunity.py
+++ suntek_app/suntek/custom/opportunity.py
@@ -5,9 +5,10 @@ def change_opportunity_status(doc,method):
     
 
 def set_opportunity_name(doc,method):
-
     if doc.name:
         doc.custom_opportunity_name = doc.name
+    if doc.contact_person:
+        doc.customer=doc.contact_person
 
 
 @frappe.whitelist()
