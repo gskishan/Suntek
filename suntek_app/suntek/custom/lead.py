4a545b84888ab63792cba551cf13afa9042a22d7
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-29 12:10:58 +0530

    Update lead.py

--- suntek_app/suntek/custom/lead.py
+++ suntek_app/suntek/custom/lead.py
@@ -1,22 +1,22 @@
 import frappe
 
 def change_enquiry_status(doc,method):
-    
-    if doc.custom_enquiry_status:
-        doc.status = doc.custom_enquiry_status
-    duplicate_check(doc)
+	
+	if doc.custom_enquiry_status:
+		doc.status = doc.custom_enquiry_status
+	duplicate_check(doc)
 
 def set_enquiry_name(doc,method):
 
-    if doc.name:
-        doc.custom_enquiry_name = doc.name
+	if doc.name:
+		doc.custom_enquiry_name = doc.name
 
 
 @frappe.whitelist()
 def custom_make_opportunity(source_name, target_doc=None):
 	def set_missing_values(source, target):
 		_set_missing_values(source, target)
-        target.custom_enquiry_status="Open"
+		target.custom_enquiry_status="Open"
 
 	target_doc = get_mapped_doc(
 		"Lead",
@@ -45,14 +45,14 @@ def custom_make_opportunity(source_name, target_doc=None):
 
 
 def duplicate_check(doc):
-    leads = frappe.db.get_list('Lead',
-    filters={
-        'mobile_no': doc.mobile_no,
-        'name': ('!=', doc.name)  
-    },
-    fields=['name', 'mobile_no'],
-    as_list=True
-    )
+	leads = frappe.db.get_list('Lead',
+	filters={
+		'mobile_no': doc.mobile_no,
+		'name': ('!=', doc.name)  
+	},
+	fields=['name', 'mobile_no'],
+	as_list=True
+	)
 
-    if leads:
-        frappe.throw("Duplicate Mobile no {0}".format(doc.mobile_no))
+	if leads:
+		frappe.throw("Duplicate Mobile no {0}".format(doc.mobile_no))
