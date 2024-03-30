45893c0338c9ce234c064a32cdd803676166cdd7
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-30 15:02:01 +0530

    Update opportunity.py

--- suntek_app/suntek/custom/opportunity.py
+++ suntek_app/suntek/custom/opportunity.py
@@ -1,15 +1,17 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
 def change_opportunity_status(doc,method):
-    pass
-    
+	pass
+	
 
 def set_opportunity_name(doc,method):
-    if doc.name:
-        doc.custom_opportunity_name = doc.name
-    if doc.contact_person:
-        doc.customer=doc.contact_person
-
+	if doc.name:
+		doc.custom_opportunity_name = doc.name
+	if doc.contact_person:
+		doc.customer=doc.contact_person
+	if doc.custom_customer_category=='C & I':
+		doc.customer=doc.custom_company_name
+		
 
 @frappe.whitelist()
 def custom_make_customer(source_name, target_doc=None):
