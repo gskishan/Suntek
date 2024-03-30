6d7468dd1cd06bddc4e97bc5dfb890cbd3dba99c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-30 17:40:32 +0530

    Update quotation.py

--- suntek_app/custom_script/quotation.py
+++ suntek_app/custom_script/quotation.py
@@ -1,4 +1,35 @@
 import frappe
+from erpnext.selling.doctype.quotation.quotation import Quotation
+from erpnext.selling.doctype.quotation.quotation import *
+class CustomQuotation (Quotation):
+	def validate(self):
+		super(Quotation, self).validate()
+		self.set_status()
+		self.validate_uom_is_integer("stock_uom", "stock_qty")
+		self.validate_uom_is_integer("uom", "qty")
+		self.validate_valid_till()
+		self.validate_shopping_cart_items()
+		self.set_custom_customer_name()
+		if self.items:
+			self.with_items = 1
+
+		from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
+
+		make_packing_list(self)
+        
+	def set_custom_customer_name(self):
+		if self.party_name and self.quotation_to == "Customer":
+			self.customer_name = frappe.db.get_value("Customer", self.party_name, "customer_name")
+		elif self.party_name and self.quotation_to == "Lead":
+			lead_name, company_name = frappe.db.get_value(
+				"Lead", self.party_name, ["lead_name", "company_name"]
+			)
+			pass
+
+
+    
+
+
 @frappe.whitelist()
 def validate(self,method):
     pass
