7497b400ba6085f07ab0dc72acc7f467a6e96d6e
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-06 17:20:48 +0530

    Update delivery_note.js

--- suntek_app/public/js/delivery_note.js
+++ suntek_app/public/js/delivery_note.js
@@ -1,4 +1,31 @@
 frappe.ui.form.on("Delivery Note", {
+     refresh: function (frm, doc, dt, dn) {
+        frm.add_custom_button(__('Designing'),
+            function () {
+                if (!cur_frm.doc.project) {
+                    frappe.throw({
+                        title: __("Mandatory"),
+                        message: __("Please Select a Project")
+                    });
+                }
+                else {
+                erpnext.utils.map_current_doc({
+                    method: "suntek_app.suntek.doctype.designing.designing.get_item",
+                    source_doctype: "Designing",
+                    target: frm,
+                    setters: {
+                        project: frm.doc.project,
+                    },
+                    get_query_filters: {
+                        customer: frm.doc.project,
+                    },
+
+                }) 
+                }
+
+            }, __("Get Items From"));
+
+    },
 
 
 })
