b739d887918c48d6f3498c9216e51dfa5ef5229f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-06 20:00:22 +0530

    Update project.py

--- suntek_app/suntek/custom/project.py
+++ suntek_app/suntek/custom/project.py
@@ -1,7 +1,38 @@
 
 
+import frappe
+from frappe import _
+
 @frappe.whitelist()
-def validate(doc,method):
-  if doc.is_new():
+def on_update(doc,method):
+	if doc.custom_type_of_case == "Subsidy":
+		if not doc.custom_discom_id:
+			
+			discomDoc = frappe.new_doc('Discom');
+			discomDoc.project_name = doc.name
+			discomDoc.sales_order = doc.sales_order
+			discomDoc.customer_name =doc.customer
+			discomDoc.save()
+
+
+            
+                      
+		if not doc.custom_subsidy_id:
+			subsidyDoc = frappe.new_doc('Subsidy')
+			subsidyDoc.project_name = doc.name
+			subsidyDoc.sales_order = doc.sales_order
+			subsidyDoc.customer_name = doc.customer
+			subsidyDoc.save()
+                       
+	elif doc.custom_type_of_case == "Non Subsidy":
+		if not doc.custom_discom_id:
+
+			discomDoc = frappe.new_doc('Discom')
+			discomDoc.project_name = doc.name
+			discomDoc.sales_order = doc.sales_order
+			discomDoc.customer_name =doc.customer
+			discomDoc.save()
+            
+        
     
             
