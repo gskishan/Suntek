bf85745844c5add3ec7cfbf4acb4405ec4a8da57
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-07 16:48:01 +0530

    Update site_survey.py

--- suntek_app/suntek/doctype/site_survey/site_survey.py
+++ suntek_app/suntek/doctype/site_survey/site_survey.py
@@ -25,6 +25,25 @@ class SiteSurvey(Document):
 
     def update_site_survery_status(self):
         self.site_survey_status = "Site Survey Completed"
+        
+    @frappe.whitelist()	
+    def get_opportunity_details(self):
+        if self.is_new() and self.custom_project:
+            so= frappe.db.get_value('Project', self.custom_project, 'sales_order')
+            opportunity= frappe.db.get_value('Sales Order', so, 'custom_opportunity_name')
+            op=frappe.get_doc("Opportunity", opportunity)
+            self.opportunity_name=op.name
+            if op.custom_customer_category=="Individual":
+                self.customer_name=op.contact_person
+            if op.custom_customer_category=='C & I':
+                self.customer_name=op.custom_company_name
+            self.customer_number=op.contact_mobile
+            self.opportunity_owner=op.opportunity_owner
+            self.sales_person=op.custom_sales_excecutive
+            self.poc_name=op.custom_person_name
+            self.poc_contact=op.custom_another_mobile_no
+		    
+            
        
 
     def update_opportunity_status_section(self):
