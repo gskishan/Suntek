b8bd8c426fcc71d0a633b7b4337d3ed962b6787d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-07 16:32:00 +0530

    Update site_survey.js

--- suntek_app/suntek/doctype/site_survey/site_survey.js
+++ suntek_app/suntek/doctype/site_survey/site_survey.js
@@ -1,34 +1,45 @@
 frappe.ui.form.on("Site Survey", {
-    refresh: function(frm) {
-    
-        frm.add_custom_button(__('Designing'), function() {
-    
-            frappe.model.with_doctype('Designing', function() {
-                var DesigningDoc = frappe.model.get_new_doc('Designing');
-                console.log(frm.doc.site_engineer)
-				DesigningDoc.designer = frm.doc.site_engineer
-				DesigningDoc.site_survey = frm.doc.name;
-                DesigningDoc.opportunity_name = frm.doc.opportunity_name;
-                DesigningDoc.designing_status = "Open"
-                
+    refresh: function (frm) {
+        if (frm.is_new()){
 
-                // Open the Site Survey document for editing
-                frappe.set_route('Form', 'Designing', DesigningDoc.name);
-
-            });
-        }, __('Create'));
-        frm.fields_dict['site_engineer'].get_query = function(doc, cdt, cdn) {
-            return {
-                filters: {
-                    'department': frm.doc.department
+            frappe.call({
+                method: "get_opportunity_details",
+                doc: frm.doc,
+                callback: function (e) {
+                    cur_frm.refresh_fields()
                 }
-            };
+            })
+        }
+    
+    
+        frm.add_custom_button(__('Designing'), function () {
+
+        frappe.model.with_doctype('Designing', function () {
+            var DesigningDoc = frappe.model.get_new_doc('Designing');
+            console.log(frm.doc.site_engineer)
+            DesigningDoc.designer = frm.doc.site_engineer
+            DesigningDoc.site_survey = frm.doc.name;
+            DesigningDoc.opportunity_name = frm.doc.opportunity_name;
+            DesigningDoc.designing_status = "Open"
+
+
+            // Open the Site Survey document for editing
+            frappe.set_route('Form', 'Designing', DesigningDoc.name);
+
+        });
+    }, __('Create'));
+    frm.fields_dict['site_engineer'].get_query = function (doc, cdt, cdn) {
+        return {
+            filters: {
+                'department': frm.doc.department
+            }
         };
-        frm.fields_dict['department'].df.onchange = function() {
-            frm.set_value('site_engineer', '');
-            frm.refresh_field('site_engineer');
-        };
-    },
+    };
+    frm.fields_dict['department'].df.onchange = function () {
+        frm.set_value('site_engineer', '');
+        frm.refresh_field('site_engineer');
+    };
+},
    
 });
 
