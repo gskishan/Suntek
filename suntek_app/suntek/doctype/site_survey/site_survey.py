df5c79a32ce1646a75984013b36ecb95a906342e
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-08 15:11:21 +0530

    Update site_survey.py

--- suntek_app/suntek/doctype/site_survey/site_survey.py
+++ suntek_app/suntek/doctype/site_survey/site_survey.py
@@ -29,19 +29,17 @@ class SiteSurvey(Document):
     @frappe.whitelist()	
     def get_opportunity_details(self):
         if self.is_new() and self.custom_project:
-            so= frappe.db.get_value('Project', self.custom_project, 'sales_order')
+            project_doc=frappe.get_doc("Project",self.custom_project)
+            so= project_doc.sales_order
             opportunity= frappe.db.get_value('Sales Order', so, 'custom_opportunity_name')
             op=frappe.get_doc("Opportunity", opportunity)
             self.opportunity_name=op.name
-            if op.custom_customer_category=="Individual":
-                self.customer_name=op.contact_person
-            if op.custom_customer_category=='C & I':
-                self.customer_name=op.custom_company_name
-            self.customer_number=op.contact_mobile
+            self.customer_name=project_doc.customer
+            self.customer_number=project_doc.custom_customer_mobile
             self.opportunity_owner=op.opportunity_owner
             self.sales_person=op.custom_sales_excecutive
-            self.poc_name=op.custom_person_name
-            self.poc_contact=op.custom_another_mobile_no
+            self.poc_name=project_doc.custom_person_name
+            self.poc_contact=project_doc.custom_another_mobile_no
             if op.customer_address:
                 formattedAddress=frappe.get_doc("Address", op.customer_address)
                 self.site_location = (
