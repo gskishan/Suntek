427925a3bc5f94daa845406884f1be6e61ed28b3
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-08 19:32:09 +0530

    Update sales_order.py

--- suntek_app/suntek/custom/sales_order.py
+++ suntek_app/suntek/custom/sales_order.py
@@ -4,11 +4,49 @@ from erpnext.selling.doctype.sales_order.sales_order import make_project
 
 @frappe.whitelist()
 def auto_project_creation_on_submit(doc,method):
-    project_make = make_project(doc)
-    project_make.custom_poc_person_name=doc.custom_poc_person_name
-    project_make.custom_poc_mobile_no=doc.custom_poc_mobile_no
-    project_make.save()
-    update_opportunity(doc)
+	project_make = make_project(doc)
+	project_make.custom_poc_person_name=doc.custom_poc_person_name
+	project_make.custom_poc_mobile_no=doc.custom_poc_mobile_no
+	project_make.save()
+	create_subsidy(doc)
+	update_opportunity(doc)
+
+def create_subsidy(doc):
+	if doc.custom_type_of_case == "Subsidy":
+		if not doc.custom_discom_id:
+			if not frappe.db.get_value('Discom', {'project_name': doc.name}, ['sales_order', 'name'])
+			
+				discomDoc = frappe.new_doc('Discom');
+				discomDoc.project_name = doc.name
+				discomDoc.sales_order = doc.sales_order
+				discomDoc.customer_name =doc.customer
+				discomDoc.save()
+
+
+            
+                      
+		if not doc.custom_subsidy_id:
+			if not frappe.db.get_value('Subsidy', {'project_name': doc.name}, ['sales_order', 'name'])
+				frappe.errprint([doc.is_new(),doc.docstatus])
+				subsidyDoc = frappe.new_doc('Subsidy')
+				subsidyDoc.project_name = doc.name
+				subsidyDoc.sales_order = doc.sales_order
+				subsidyDoc.customer_name = doc.customer
+				subsidyDoc.save()
+                       
+	elif doc.custom_type_of_case == "Non Subsidy":
+		if not doc.custom_discom_id:
+			if not frappe.db.get_value('Discom', {'project_name': doc.name}, ['sales_order', 'name'])
+				discomDoc = frappe.new_doc('Discom')
+				discomDoc.project_name = doc.name
+				discomDoc.sales_order = doc.sales_order
+				discomDoc.customer_name =doc.customer
+				discomDoc.save()
+            
+        
+    
+            
+
 
 def update_opportunity(doc):
     if doc.items[0].quotation_item:
