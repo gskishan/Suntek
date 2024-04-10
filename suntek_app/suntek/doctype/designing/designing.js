5acb9614a97374e0d3f5b1086a3179c8c57a84f4
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-10 12:48:53 +0530

    Update designing.js

--- suntek_app/suntek/doctype/designing/designing.js
+++ suntek_app/suntek/doctype/designing/designing.js
@@ -2,34 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Designing', {
-    if (frm.is_new()){
+	refresh: function (frm) {
+		if (frm.is_new()) {
 
-            frappe.call({
-                method: "get_opportunity_details",
-                doc: frm.doc,
-                callback: function (e) {
-                    cur_frm.refresh_fields()
-                }
-            })
-        }
-    
+			frappe.call({
+				method: "get_opportunity_details",
+				doc: frm.doc,
+				callback: function (e) {
+					cur_frm.refresh_fields()
+				}
+			})
+		}
+	},
 
 
-    });
+
+});
 
 frappe.ui.form.on('Designing Item', {
-    rate: function(frm, cdt, cdn) {
-        var child = locals[cdt][cdn];
-        calculate_amount(child);
-    },
+	rate: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		calculate_amount(child);
+	},
 
-    qty: function(frm, cdt, cdn) {
-        var child = locals[cdt][cdn];
-        calculate_amount(child);
-    }
+	qty: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		calculate_amount(child);
+	}
 });
 
 function calculate_amount(child) {
-    child.amount = flt(child.rate) * flt(child.qty);
-    refresh_field('amount', child.name, 'bom');
+	child.amount = flt(child.rate) * flt(child.qty);
+	refresh_field('amount', child.name, 'bom');
 }
