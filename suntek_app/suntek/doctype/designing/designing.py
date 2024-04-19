7120a193ae7a4d7db7160397a15433065235413d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-19 22:07:40 +0530

    Update designing.py

--- suntek_app/suntek/doctype/designing/designing.py
+++ suntek_app/suntek/doctype/designing/designing.py
@@ -153,7 +153,7 @@ def make_stock_entry(source_name, target_doc=None):
 		target.set_transfer_qty()
 		target.set_actual_qty()
 		target.calculate_rate_and_amount(raise_error_if_no_rate=False)
-		target.stock_entry_type = target.purpose
+		target.stock_entry_type = "Material Transfer to Customer"
 		target.set_job_card_data()
 
 	doclist = get_mapped_doc("Designing", source_name, {
