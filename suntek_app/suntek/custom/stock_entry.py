d7c038dd10c9a59af432480acef72ffa04f6e786
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-25 15:13:01 +0530

    Update stock_entry.py

--- suntek_app/suntek/custom/stock_entry.py
+++ suntek_app/suntek/custom/stock_entry.py
@@ -1,14 +1,16 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
 def on_submit(doc,method):
-	for d in doc.items:
-		if d.against_designing_item and d.against_designing:
-			desgn=frappe.get_doc("Designing Item",d.against_designing_item)
-			desgn.db_set("transferred",desgn.transferred+d.qty)
+	if doc.stock_entry_type=='Material Transfer to Customer':
+		for d in doc.items:
+			if d.against_designing_item and d.against_designing:
+				desgn=frappe.get_doc("Designing Item",d.against_designing_item)
+				desgn.db_set("transferred",desgn.transferred+d.qty)
 
 
 def on_cancel(doc,method):
-	for d in doc.items:
-		if d.against_designing_item and d.against_designing:
-			desgn=frappe.get_doc("Designing Item",d.against_designing_item)
-			desgn.db_set("transferred",desgn.transferred-d.qty)
+	if doc.stock_entry_type=='Material Transfer to Customer':
+		for d in doc.items:
+			if d.against_designing_item and d.against_designing:
+				desgn=frappe.get_doc("Designing Item",d.against_designing_item)
+				desgn.db_set("transferred",desgn.transferred-d.qty)
