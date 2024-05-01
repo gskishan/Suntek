abbb7456cefd38bb15058706eb1d379b0266b005
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-01 14:03:59 +0530

    Update salary_slip.py

--- suntek_app/custom_script/salary_slip.py
+++ suntek_app/custom_script/salary_slip.py
@@ -1,3 +1,22 @@
 import frappe
 from frappe import _
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+from hrms.payroll.doctype.salary_slip.salary_slip import *
+from frappe.utils import (	flt)
+
+class CustomSalarySlip(SalarySlip):
+	def pull_sal_struct(self):
+		from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
+
+		if self.salary_slip_based_on_timesheet:
+			self.salary_structure = self._salary_structure_doc.name
+			self.hour_rate = 11
+			self.base_hour_rate = flt(self.hour_rate) * flt(self.exchange_rate)
+			self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
+			wages_amount = self.hour_rate * self.total_working_hours
+
+			self.add_earning_for_hourly_wages(
+				self, self._salary_structure_doc.salary_component, wages_amount
+			)
+		frappe.errprint("workingcustom")
+		make_salary_slip(self._salary_structure_doc.name, self)
