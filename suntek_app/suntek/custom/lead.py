e2292a5d075193ed63220cc2c359f7765e06e8a6
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-07 18:29:28 +0530

    Update lead.py

--- suntek_app/suntek/custom/lead.py
+++ suntek_app/suntek/custom/lead.py
@@ -100,7 +100,6 @@ def duplicate_check(doc):
 	if leads:
 		# lead = leads[0]
 		frappe.errprint(leads)
-		frappe.throw("Duplicate Mobile no")
 		frappe.throw(_("Duplicate Mobile no {0} {1}").format(
 			doc.mobile_no,
 			get_link_to_form("Lead", lead[0][0])
