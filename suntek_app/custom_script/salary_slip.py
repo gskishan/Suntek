bc7e748719eec1563dbaeb64e7d4739e4b99a30f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-07 10:28:10 +0530

    Update salary_slip.py

--- suntek_app/custom_script/salary_slip.py
+++ suntek_app/custom_script/salary_slip.py
@@ -13,7 +13,7 @@ class CustomSalarySlip(SalarySlip):
 			self.salary_structure = self._salary_structure_doc.name
 			self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
 			base=get_base_amount(self.employee)
-			rt = round((base / self.total_working_days) / 8.5)
+			rt = ((base / self.total_working_days) / 8.5)
 			self.hour_rate = rt
 			self.base_hour_rate = flt(self.hour_rate) * flt(self.exchange_rate)
 			wages_amount = self.hour_rate * self.total_working_hours
@@ -23,7 +23,7 @@ class CustomSalarySlip(SalarySlip):
 		make_salary_slip(self._salary_structure_doc.name, self)
 		if self.salary_slip_based_on_timesheet:
 			base=get_base_amount(self.employee)
-			rt = round((base / self.total_working_days) / 8.5)
+			rt = ((base / self.total_working_days) / 8.5)
 			self.hour_rate = rt
 		
 		
