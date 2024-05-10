0c9a6b4bce308600206683d654bb8a5662ccfd7f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-10 12:12:48 +0530

    Update hooks.py

--- suntek_app/hooks.py
+++ suntek_app/hooks.py
@@ -41,6 +41,7 @@ doctype_js = {
     "Delivery Note": "public/js/delivery_note.js",
     "Material Request": "public/js/material_request.js",
     "Stock Entry": "public/js/stock_entry.js",
+  "BOM": "public/js/bom.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
