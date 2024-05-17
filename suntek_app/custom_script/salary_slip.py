c1463e16b6803ad62ed7c50b5f3742def0615bf5
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-17 20:32:00 +0530

    Update salary_slip.py

--- suntek_app/custom_script/salary_slip.py
+++ suntek_app/custom_script/salary_slip.py
@@ -3,8 +3,46 @@ from frappe import _
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
 from hrms.payroll.doctype.salary_slip.salary_slip import *
 from frappe.utils import flt
+from hrms.hr.utils import  validate_active_employee
 
 class CustomSalarySlip(SalarySlip):
+	def validate(self):
+		self.status = self.get_status()
+		validate_active_employee(self.employee)
+		self.validate_dates()
+		self.check_existing()
+		frappe.errprint(len(self.deductions))
+		if not self.salary_slip_based_on_timesheet:
+			self.get_date_details()
+
+		if not (len(self.get("earnings")) or len(self.get("deductions"))):
+			# get details from salary structure
+			frappe.errprint(len(self.deductions))
+			self.get_emp_and_working_day_details()
+			frappe.errprint(len(self.deductions))
+		else:
+			self.get_working_days_details(lwp=self.leave_without_pay)
+		frappe.errprint(len(self.deductions))
+		self.calculate_net_pay()
+		self.compute_year_to_date()
+		self.compute_month_to_date()
+		self.compute_component_wise_year_to_date()
+		self.add_leave_balances()
+		self.compute_income_tax_breakup()
+
+		if frappe.db.get_single_value("Payroll Settings", "max_working_hours_against_timesheet"):
+			max_working_hours = frappe.db.get_single_value(
+				"Payroll Settings", "max_working_hours_against_timesheet"
+			)
+			if self.salary_slip_based_on_timesheet and (self.total_working_hours > int(max_working_hours)):
+				frappe.msgprint(
+					_("Total working hours should not be greater than max working hours {0}").format(
+						max_working_hours
+					),
+					alert=True,
+				)
+
+
 	@frappe.whitelist()
 	def pull_sal_struct(self):
 		from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
