5b601a0f456de1da0c9edbf2090451cc961c2787
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-21 11:05:43 +0530

    Update salary_slip.py

--- suntek_app/custom_script/salary_slip.py
+++ suntek_app/custom_script/salary_slip.py
@@ -1,49 +1,47 @@
 import frappe
 from frappe import _
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+from hrms.payroll.doctype.salary_slip.salary_slip import *
 from frappe.utils import flt
-from hrms.hr.utils import validate_active_employee
+from hrms.hr.utils import  validate_active_employee
 
 class CustomSalarySlip(SalarySlip):
 
-    @frappe.whitelist()
-    def pull_sal_struct(self):
-        from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
+	@frappe.whitelist()
+	def pull_sal_struct(self):
+		from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
+		rt=0
+		if self.salary_slip_based_on_timesheet:
+			self.salary_structure = self._salary_structure_doc.name
+			self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
+			make_salary_slip(self._salary_structure_doc.name, self)
+			deduct=0
+			for d in self.deductions:
+				if d.salary_component=='Provident Fund Employee' or  d.salary_component=='ESIC Employer':
+					deduct+=d.amount
+			
+			self.set("earnings", [])
+			self.set("deductions", [])
+			base=get_base_amount(self.employee)
+			rt = ((base / self.total_working_days) / 8.5) - deduct
+			self.hour_rate = rt
+			self.base_hour_rate = flt(self.hour_rate) * flt(self.exchange_rate)
+			wages_amount = self.hour_rate * self.total_working_hours
+			self.add_earning_for_hourly_wages(
+				self, self._salary_structure_doc.salary_component, wages_amount
+			)
 
-        rt = 0
-        if self.salary_slip_based_on_timesheet:
-            self.salary_structure = self._salary_structure_doc.name
-            self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
-            make_salary_slip(self._salary_structure_doc.name, self)
-
-            adding = 0
-            for e in self.earnings:
-                if e.salary_component in ['Basic', 'Conveyance Allowance', 'House Rent Allowance', 'Medical Allowance']:
-                    adding += e.amount
-
-            self.set("earnings", [])
-            self.set("deductions", [])
-
-            if self.total_working_days > 0:
-                rt = (adding / self.total_working_days) / 8.5
-            else:
-                frappe.throw(_("Total working days cannot be zero"))
-
-            self.hour_rate = rt
-            self.base_hour_rate = flt(self.hour_rate) * flt(self.exchange_rate)
-
-            wages_amount = self.hour_rate * self.total_working_hours
-            self.add_earning_for_hourly_wages(self._salary_structure_doc.salary_component, wages_amount)
-
-        make_salary_slip(self._salary_structure_doc.name, self)
-        if self.salary_slip_based_on_timesheet:
-            self.hour_rate = rt
+		make_salary_slip(self._salary_structure_doc.name, self)
+		if self.salary_slip_based_on_timesheet:
+			self.hour_rate = rt
+		
+		
 
 def get_base_amount(employee):
-    sql = """SELECT base FROM `tabSalary Structure Assignment` WHERE employee=%s"""
-    base = frappe.db.sql(sql, employee, as_dict=True)
-    if base:
-        return base[0].base
-    else:
-        frappe.msgprint(_("Issue in finding salary assignment"))
-        return 0
+	sql="""select base from `tabSalary Structure Assignment` where employee="{0}" """.format(employee)
+	base=frappe.db.sql(sql,as_dict=True)
+	if base:
+		return base[0].base
+	else:
+		frappe.msgprint("issue in finding salary assigment")
+		return 0
