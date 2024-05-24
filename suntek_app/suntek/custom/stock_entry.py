c32396d45585f59295da2c3358e61357bf57469d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-24 11:48:15 +0530

    Update stock_entry.py

--- suntek_app/suntek/custom/stock_entry.py
+++ suntek_app/suntek/custom/stock_entry.py
@@ -13,13 +13,15 @@ def on_submit(doc,method):
 	if doc.stock_entry_type=='Material Transfer to Customer':
 		for d in doc.items:
 			if d.against_designing_item and d.against_designing:
-				desgn=frappe.get_doc("Designing Item",d.against_designing_item)
-				desgn.db_set("transferred",desgn.transferred+d.qty)
+				if frappe.db.exists("Designing Item", d.against_designing_item):
+					desgn=frappe.get_doc("Designing Item",d.against_designing_item)
+					desgn.db_set("transferred",desgn.transferred+d.qty)
 
 
 def on_cancel(doc,method):
 	if doc.stock_entry_type=='Material Transfer to Customer':
 		for d in doc.items:
 			if d.against_designing_item and d.against_designing:
-				desgn=frappe.get_doc("Designing Item",d.against_designing_item)
-				desgn.db_set("transferred",desgn.transferred-d.qty)
+				if frappe.db.exists("Designing Item", d.against_designing_item):
+					desgn=frappe.get_doc("Designing Item",d.against_designing_item)
+					desgn.db_set("transferred",desgn.transferred-d.qty)
