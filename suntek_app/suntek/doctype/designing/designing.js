498c17507f7a9bef0bd7f187b22b32ddddcaf505
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-06 15:05:22 +0530

    Update designing.js

--- suntek_app/suntek/doctype/designing/designing.js
+++ suntek_app/suntek/doctype/designing/designing.js
@@ -18,7 +18,8 @@ frappe.ui.form.on('Designing', {
 		cur_frm.set_query("designer", function (frm) {
 			return {
 				filters: [
-					['Employee', 'designation', '=', "Autocad draft man"]
+					['Employee', 'designation', '=', "Autocad draft man"],
+					['Employee', 'designation', '=', "Autocad Draft Woman"]
 				]
 			}
 		});
