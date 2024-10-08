af0d0b60454e025d294e1ff9dfb5e3e47d84792f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-10-08 15:20:39 +0530

    Update project_wise_summary.js

--- suntek_app/suntek/report/project_wise_summary/project_wise_summary.js
+++ suntek_app/suntek/report/project_wise_summary/project_wise_summary.js
@@ -1,1 +1,45 @@
 
+frappe.query_reports["Project Wise Summary"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default":frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			
+		},
+		
+		{
+			"fieldname":"project",
+			"label": __("Project"),
+			"fieldtype": "Link",
+			"options": "Project",
+            
+		},
+        {
+			"fieldname":"customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+            
+		},
+		
+	
+		
+
+]
+};
