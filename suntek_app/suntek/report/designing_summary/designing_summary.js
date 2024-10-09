a1e0718bf62579ce9569e92a7eccd6c5c5e51b07
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-10-09 13:03:52 +0530

    Create designing_summary.js

---
+++ suntek_app/suntek/report/designing_summary/designing_summary.js
@@ -0,0 +1,24 @@
+
+frappe.query_reports["Designing Summary"] = {
+	"filters": [
+	
+		{
+			"fieldname":"project",
+			"label": __("Project"),
+			"fieldtype": "Link",
+			"options": "Project",
+            
+		},
+        {
+			"fieldname":"designing",
+			"label": __("Designing"),
+			"fieldtype": "Link",
+			"options": "Designing",
+            
+		},
+		
+	
+		
+
+]
+};
