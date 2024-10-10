4a496acf399f36fce607df7cb473d730007def10
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-10-10 18:13:56 +0530

    Update designing_summary.js

--- suntek_app/suntek/report/designing_summary/designing_summary.js
+++ suntek_app/suntek/report/designing_summary/designing_summary.js
@@ -1,24 +1,34 @@
 
 frappe.query_reports["Designing Summary"] = {
-	"filters": [
-	
-		{
-			"fieldname":"project",
-			"label": __("Project"),
-			"fieldtype": "Link",
-			"options": "Project",
-            
-		},
-        {
-			"fieldname":"designing",
-			"label": __("Designing"),
-			"fieldtype": "Link",
-			"options": "Designing",
-            
-		},
-		
-	
-		
+    "filters": [
 
-]
+        {
+            "fieldname": "project",
+            "label": __("Project"),
+            "fieldtype": "Link",
+            "options": "Project",
+
+        },
+        {
+            "fieldname": "designing",
+            "label": __("Designing"),
+            "fieldtype": "Link",
+            "options": "Designing",
+            get_query: function () {
+                let project = frappe.query_report.get_filter_value("project");
+                if (!project) return;
+
+                return {
+                    query: "suntek.suntek.designing_summary.designing_summary.get_designing_options",
+                    filters: {
+                        project: project
+                    }
+                };
+            }
+        }
+
+
+
+
+    ]
 };
