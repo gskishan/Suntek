02ba00acde1d52493a8d5239e32fbdb72a488db6
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-11-04 10:38:49 +0530

    Update quotation.py

--- suntek_app/custom_script/quotation.py
+++ suntek_app/custom_script/quotation.py
@@ -1,46 +1,45 @@
 import frappe
 from erpnext.selling.doctype.quotation.quotation import Quotation
-from erpnext.selling.doctype.quotation.quotation import *
-class CustomQuotation (Quotation):
-	def validate(self):
-		super(Quotation, self).validate()
-		self.set_status()
-		self.validate_uom_is_integer("stock_uom", "stock_qty")
-		self.validate_uom_is_integer("uom", "qty")
-		self.validate_valid_till()
-		self.validate_shopping_cart_items()
-		self.set_custom_customer_name()
-		if self.items:
-			self.with_items = 1
+from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
 
-		from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
+class CustomQuotation(Quotation):
+    def validate(self):
+        # Calling the base Quotation's validate method
+        super(CustomQuotation, self).validate()
 
-		make_packing_list(self)
-        
-	def set_custom_customer_name(self):
-		if self.party_name and self.quotation_to == "Customer":
-			self.customer_name = frappe.db.get_value("Customer", self.party_name, "customer_name")
-		elif self.party_name and self.quotation_to == "Lead":
-			lead_name, company_name = frappe.db.get_value(
-				"Lead", self.party_name, ["lead_name", "company_name"]
-			)
-			pass
+        # Custom validations and assignments
+        self.set_status()
+        self.validate_uom_is_integer("stock_uom", "stock_qty")
+        self.validate_uom_is_integer("uom", "qty")
+        self.validate_valid_till()
 
+        # Only call validate_shopping_cart_items() if defined in Quotation
+        if hasattr(self, 'validate_shopping_cart_items'):
+            self.validate_shopping_cart_items()
 
-    
+        # Setting custom customer name if the quotation is to a customer or lead
+        self.set_custom_customer_name()
 
+        # Setting `with_items` if there are items in the quotation
+        if self.items:
+            self.with_items = 1
 
-@frappe.whitelist()
-def validate(self,method):
-    pass
-    #for d in self.items:
-        #d.custom_total_gst_rate=d.igst_rate+d.cgst_rate+d.sgst_rate
-        #d.custom_total_gst_amount=d.igst_amount+d.cgst_amount+d.sgst_amount
+        # Generating a packing list based on items
+        make_packing_list(self)
 
+    def set_custom_customer_name(self):
+        # Custom method to set customer name based on quotation_to
+        if self.party_name:
+            if self.quotation_to == "Customer":
+                self.customer_name = frappe.db.get_value("Customer", self.party_name, "customer_name")
+            elif self.quotation_to == "Lead":
+                lead_name, company_name = frappe.db.get_value("Lead", self.party_name, ["lead_name", "company_name"])
+                self.customer_name = company_name or lead_name
 
 @frappe.whitelist()
 def get_grandtotal(quotation=None):
+    # Fetch and return grand total for a specified quotation
     if quotation:
-        qt=frappe.get_doc("Quotation",quotation)
-        gt_grand=qt.grand_total
-        return gt_grand
+        qt = frappe.get_doc("Quotation", quotation)
+        return qt.grand_total
+    return None
