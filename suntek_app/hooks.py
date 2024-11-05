91603dc31d1b7a4bdd6d3d5c7cd4a29163996827
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-11-05 16:34:56 +0530

    Update hooks.py

--- suntek_app/hooks.py
+++ suntek_app/hooks.py
@@ -153,6 +153,7 @@ doc_events = {
     },
     "Sales Order": {
         "on_submit":"suntek_app.suntek.custom.sales_order.auto_project_creation_on_submit",
+	    "on_update":"suntek_app.suntek.custom.sales_order.on_update",
 	# "validate":"suntek_app.suntek.custom.sales_order.validate"
     },
    "Project": {
