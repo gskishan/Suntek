248f0dd4832e666eca2c98f49727cc6fca7493c8
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-11-05 13:57:55 +0530

    Update sales_order.py

--- suntek_app/suntek/custom/sales_order.py
+++ suntek_app/suntek/custom/sales_order.py
@@ -1,24 +1,38 @@
 import frappe
-#from erpnext.selling.doctype.sales_order.sales_order import make_project
+from erpnext.selling.doctype.sales_order.sales_order import make_project
 
-@frappe.whitelist()
-def auto_project_creation_on_submit(doc, method):
-    # Ensure the document is submitted before creating a project
+def on_update(doc):
     if doc.docstatus == 1 and not doc.amended_from:
-        # Create Project from Sales Order
+    # Create Project from Sales Order
         if not frappe.db.exists("Project", {"project_name": doc.name}):
-
             project_make = make_project(doc)
             project_make.custom_poc_person_name = doc.custom_person_name
             project_make.custom_poc_mobile_no = doc.custom_another_mobile_no
             project_make.save()
-        
-        # Create subsidy or discom records if applicable
+
         create_subsidy_or_discom(project_make)
     elif doc.amended_from and doc.project:
-        # Update existing project if present
         project = frappe.get_doc("Project", doc.project)
         project.db_set("sales_order", doc.name)
+
+@frappe.whitelist()
+def auto_project_creation_on_submit(doc, method):
+    # Ensure the document is submitted before creating a project
+    # if doc.docstatus == 1 and not doc.amended_from:
+    #     # Create Project from Sales Order
+    #     if not frappe.db.exists("Project", {"project_name": doc.name}):
+
+    #         project_make = make_project(doc)
+    #         project_make.custom_poc_person_name = doc.custom_person_name
+    #         project_make.custom_poc_mobile_no = doc.custom_another_mobile_no
+    #         project_make.save()
+        
+    #     # Create subsidy or discom records if applicable
+    #     create_subsidy_or_discom(project_make)
+    # elif doc.amended_from and doc.project:
+    #     # Update existing project if present
+    #     project = frappe.get_doc("Project", doc.project)
+    #     project.db_set("sales_order", doc.name)
     
     # Update opportunity linked with the Sales Order
     update_opportunity(doc)
@@ -61,30 +75,3 @@ def update_opportunity(doc):
 
 
 
-@frappe.whitelist()
-def make_project(source_name, target_doc=None):
-    from frappe.model.mapper import get_mapped_doc
-    def postprocess(source, doc):
-        doc.project_type = "External"
-        doc.project_name = source_name.name
-
-    doc = get_mapped_doc(
-        "Sales Order",
-        source_name,
-        {
-            "Sales Order": {
-                "doctype": "Project",
-                # Allow docstatus to be either 0 or 1
-                "validation": {"docstatus": ["in", [0, 1]]},
-                "field_map": {
-                    "name": "sales_order",
-                    "base_grand_total": "estimated_costing",
-                    "net_total": "total_sales_amount",
-                },
-            },
-        },
-        target_doc,
-        postprocess,
-    )
-
-    return doc
