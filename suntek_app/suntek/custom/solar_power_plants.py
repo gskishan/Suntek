caa8f10a044aa05daca5f6f8298e3fdde1a0374a
Aditya Singh <adityasingh14102001@gmail.com>
2025-01-02 13:38:56 +0530

    Add Solar Power Plant doctype

---
+++ suntek_app/suntek/custom/solar_power_plants.py
@@ -0,0 +1,11 @@
+def change_power_plant_assigned_status(doc, method):
+    if check_customer_details(doc, method):
+        doc.status = "Assigned"
+    else:
+        doc.status = "Unassigned"
+
+
+def check_customer_details(doc):
+    if doc.customer and doc.customer_mobile_no and doc.customer_email:
+        return True
+    return False
