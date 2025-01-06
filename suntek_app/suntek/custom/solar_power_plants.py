42cc5e2836429ed649e09f372e69d3359b216f17
Aditya Singh <adityasingh14102001@gmail.com>
2025-01-06 15:12:04 +0530

    Update Solar plant server script

--- suntek_app/suntek/custom/solar_power_plants.py
+++ suntek_app/suntek/custom/solar_power_plants.py
@@ -1,6 +1,17 @@
+import frappe
+
 def change_power_plant_assigned_status(doc, method):
     doc.status = "Assigned" if check_customer_details(doc) else "Unassigned"
 
 
 def check_customer_details(doc):
     return bool(doc.customer and doc.customer_mobile_no)
+
+
+def customer_contains_mobile_number(doc):
+    return bool(doc.customer_mobile_no)
+
+
+def check_customer_mobile_number(doc):
+    if not customer_contains_mobile_number(doc):
+        frappe.throw("Customer mobile number is mandatory!")
