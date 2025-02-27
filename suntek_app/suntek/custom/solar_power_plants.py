2b2bfd879126b6282d6f64ace8274b5de18fd162
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-27 11:51:36 +0530

    fix: move utility functions to solar power plant doctype class

--- suntek_app/suntek/custom/solar_power_plants.py
+++ suntek_app/suntek/custom/solar_power_plants.py
@@ -1,4 +1,3 @@
-import re
 import time
 from typing import Dict
 
@@ -7,38 +6,6 @@ import requests
 
 from suntek_app.suntek.custom.lead import parse_request_data
 from suntek_app.suntek.utils.api_handler import create_api_response
-
-
-def change_power_plant_assigned_status(doc, method):
-    doc.status = "Assigned" if check_customer_details(doc) else "Unassigned"
-
-
-def check_customer_details(doc):
-    return bool(doc.customers and len(doc.customers) > 0)
-
-
-def customer_contains_mobile_number(doc):
-    return bool(doc.customer_mobile_no)
-
-
-MOBILE_NUMBER_PATTERN = re.compile(r"^(\+91[-]?)?[6-9]\d{9}$")
-
-
-def validate_mobile_number(number):
-    return bool(MOBILE_NUMBER_PATTERN.match(str(number)))
-
-
-def check_customer_mobile_number(doc, method=None):
-    if doc.customer:
-        if not customer_contains_mobile_number(doc):
-            frappe.throw(
-                "Customer mobile number is mandatory. Please update the customer's mobile number or select a customer with a valid mobile number.",
-            )
-        else:
-            if not validate_mobile_number(doc.customer_mobile_no):
-                frappe.throw(
-                    "Invalid mobile number! Please enter a 10-digit number starting with 6, 7, 8, or 9, optionally prefixed by +91 or +91-.",
-                )
 
 
 def handle_solar_ambassador_webhook(doc, method=None):
--- suntek_app/suntek/doctype/solar_power_plants/solar_power_plants.py
+++ suntek_app/suntek/doctype/solar_power_plants/solar_power_plants.py
@@ -1,5 +1,32 @@
+import re
+
+import frappe
 from frappe.model.document import Document
 
 
 class SolarPowerPlants(Document):
-    pass
+    def before_save(self):
+        self.check_customer_mobile_number()
+        self.change_power_plant_assigned_status()
+
+    def change_power_plant_assigned_status(self):
+        self.status = "Assigned" if self.check_customer_details() else "Unassigned"
+
+    def check_customer_details(self):
+        return bool(self.customers and len(self.customers) > 0)
+
+    def validate_mobile_number(self, number):
+        MOBILE_NUMBER_PATTERN = re.compile(r"^(\+91[-]?)?[6-9]\d{9}$")
+        return bool(MOBILE_NUMBER_PATTERN.match(str(number)))
+
+    def check_customer_mobile_number(self):
+        if self.customers:
+            for customer in self.customers:
+                if not customer.mobile_no:
+                    frappe.throw(
+                        "Customer mobile number is mandatory for all customers in the table."
+                    )
+                if not self.validate_mobile_number(customer.mobile_no):
+                    frappe.throw(
+                        f"Invalid mobile number '{customer.mobile_no}' for customer {customer.suntek_customer}! Please enter a 10-digit number starting with 6, 7, 8, or 9, optionally prefixed by +91 or +91-."
+                    )
