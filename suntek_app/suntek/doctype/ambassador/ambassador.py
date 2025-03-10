0f442fbb0a398f6b6e1adedffebcd664af9611f6
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-10 15:16:58 +0530

    feat: implement create and update functionality for solar ambassadors with validation

--- suntek_app/suntek/doctype/ambassador/ambassador.py
+++ suntek_app/suntek/doctype/ambassador/ambassador.py
@@ -8,10 +8,28 @@ from frappe.model.document import Document
 class Ambassador(Document):
     def before_insert(self):
         self.validate_email()
+        self.validate_mobile_number()
+        self.validate_ifsc_code()
+
+    def validate(self):
+        self.validate_email()
+        self.validate_mobile_number()
+        self.validate_ifsc_code()
 
     def validate_email(self):
-        if not re.match(
-            "[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}",
+        if self.email_id and not re.match(
+            r"[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}",
             self.email_id,
         ):
             frappe.throw(_("Invalid Email Format"))
+
+    def validate_mobile_number(self):
+        if not self.ambassador_mobile_number:
+            frappe.throw(_("Mobile Number is mandatory"))
+
+        if not re.match(r"^[0-9]{10}$", self.ambassador_mobile_number):
+            frappe.throw(_("Mobile Number must be 10 digits"))
+
+    def validate_ifsc_code(self):
+        if self.ifsc_code and not re.match(r"^[A-Z]{4}0[A-Z0-9]{6}$", self.ifsc_code):
+            frappe.throw(_("Invalid IFSC Code Format. It should be like ABCD0123456"))
