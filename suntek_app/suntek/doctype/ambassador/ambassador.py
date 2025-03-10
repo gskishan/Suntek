c7a69a4d8c85f273fce3d90c03dc66afdf6b5fd5
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-10 16:57:40 +0530

    feat: enhance solar ambassador creation and update with additional fields and validation

--- suntek_app/suntek/doctype/ambassador/ambassador.py
+++ suntek_app/suntek/doctype/ambassador/ambassador.py
@@ -3,9 +3,13 @@ import re
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 
 class Ambassador(Document):
+    def autoname(self):
+        self.name = make_autoname("SES-AMBS-.#####")
+
     def before_insert(self):
         self.validate_email()
         self.validate_mobile_number()
@@ -15,6 +19,18 @@ class Ambassador(Document):
         self.validate_email()
         self.validate_mobile_number()
         self.validate_ifsc_code()
+        self.validate_aadhar_number()
+        self.validate_pan_number()
+
+    def validate_aadhar_number(self):
+        if self.aadhar_number and not re.match(r"^\d{12}$", self.aadhar_number):
+            frappe.throw(_("Aadhar Number must be 12 digits"))
+
+    def validate_pan_number(self):
+        if self.pan_number and not re.match(
+            r"^[A-Z]{5}[0-9]{4}[A-Z]{1}$", self.pan_number
+        ):
+            frappe.throw(_("Invalid PAN Number Format"))
 
     def validate_email(self):
         if self.email_id and not re.match(
