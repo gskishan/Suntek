7ff3bffd66ea8c3ae54688e9efe5f0c702925b98
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-12 15:19:42 +0530

    refactor: improve integration status toggle mechanism

--- suntek_app/migrate/__init__.py
+++ suntek_app/migrate/__init__.py
@@ -3,13 +3,14 @@ from suntek_app.channel_partner import (
     setup_channel_partner_parent_warehouse,
     setup_channel_partner_parent_warehouse_type,
 )
-from suntek_app.migrate.disable import solar_ambassador_integration
-from suntek_app.migrate.enable import solar_ambassador_integration
 from suntek_app.migrate.roles import create_sales_order_report_user
+from suntek_app.migrate.toggle_solar_ambassador_integration_status import (
+    toggle_solar_ambassador_integration,
+)
 
 
 def before_migrate():
-    solar_ambassador_integration()
+    toggle_solar_ambassador_integration(enable=False)
     setup_channel_partner()
     setup_channel_partner_parent_warehouse_type()
     setup_channel_partner_parent_warehouse()
@@ -17,4 +18,4 @@ def before_migrate():
 
 
 def after_migrate():
-    solar_ambassador_integration()
+    toggle_solar_ambassador_integration()
