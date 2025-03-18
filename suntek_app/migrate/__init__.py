01e728d277e9b116521dae20a55985383db8a807
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-18 18:08:46 +0530

    remove sales order report user role from migration tasks

--- suntek_app/migrate/__init__.py
+++ suntek_app/migrate/__init__.py
@@ -2,7 +2,6 @@ from suntek_app.channel_partner import (
     setup_channel_partner_parent_warehouse,
     setup_channel_partner_parent_warehouse_type,
 )
-from suntek_app.migrate.roles import create_sales_order_report_user
 from suntek_app.migrate.toggle_solar_ambassador_integration_status import (
     toggle_solar_ambassador_integration,
 )
@@ -12,7 +11,6 @@ def before_migrate():
     toggle_solar_ambassador_integration(enable=False)
     setup_channel_partner_parent_warehouse_type()
     setup_channel_partner_parent_warehouse()
-    create_sales_order_report_user()
 
 
 def after_migrate():
