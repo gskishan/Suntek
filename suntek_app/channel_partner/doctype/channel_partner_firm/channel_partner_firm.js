138da62c89c5a957ed223f78ec4c7967d56133e0
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-19 13:20:50 +0530

    fix: remove creation and modification fields

--- suntek_app/channel_partner/doctype/channel_partner_firm/channel_partner_firm.js
+++ suntek_app/channel_partner/doctype/channel_partner_firm/channel_partner_firm.js
@@ -1,9 +1,7 @@
 frappe.ui.form.on("Channel Partner Firm", {
   refresh: function (frm) {
-    // Standard Address and Contact creation
     frm.trigger("render_address_and_contact");
 
-    // Buttons to create Channel Partners
     if (!frm.is_new()) {
       frm.add_custom_button(__("Create Channel Partner"), function () {
         frappe.new_doc("Channel Partner", {
@@ -11,7 +9,6 @@ frappe.ui.form.on("Channel Partner Firm", {
         });
       });
 
-      // Button to view similar firms
       frm.add_custom_button(
         __("Show Similar Firms"),
         function () {
@@ -22,21 +19,13 @@ frappe.ui.form.on("Channel Partner Firm", {
         __("View"),
       );
     }
-
-    // Only set creation fields when the document is new
-    if (frm.is_new()) {
-      frm.set_value("created_by", frappe.session.user);
-      frm.set_value("creation_date", frappe.datetime.now_datetime());
-    }
   },
 
   render_address_and_contact: function (frm) {
-    // Render address and contact section using standard ERPNext utilities
     frappe.contacts.render_address_and_contact(frm);
   },
 
   validate: function (frm) {
-    // Validate firm_code format
     if (frm.doc.firm_code) {
       let valid_format = /^[A-Z0-9]{2,10}$/.test(frm.doc.firm_code);
       if (!valid_format) {
@@ -48,9 +37,5 @@ frappe.ui.form.on("Channel Partner Firm", {
         frappe.validated = false;
       }
     }
-
-    // Set modification details only when we're actually saving
-    frm.set_value("modified_by", frappe.session.user);
-    frm.set_value("modified_date", frappe.datetime.now_datetime());
   },
 });
