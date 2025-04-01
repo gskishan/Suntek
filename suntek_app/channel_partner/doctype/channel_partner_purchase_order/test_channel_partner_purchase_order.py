18a58b90cf151937c1b1d152cc1f9d21f9e26010
Aditya Singh <62322233+axis1410@users.noreply.github.com>
2025-04-01 18:30:33 +0530

    Revert "Fix/setup"

--- suntek_app/channel_partner/doctype/channel_partner_purchase_order/test_channel_partner_purchase_order.py
+++ suntek_app/channel_partner/doctype/channel_partner_purchase_order/test_channel_partner_purchase_order.py
@@ -1,27 +1,9 @@
-import unittest
+# Copyright (c) 2025, kishan and Contributors
+# See license.txt
 
-import frappe
-
-
-def create_item():
-    item = frappe.new_doc("Item")
-    item.item_code = "TEST-ITEM-001"
-    item.item_name = "Test Item"
-    item.item_group = "Products"
-    item.stock_uom = "Nos"
-    item.is_stock_item = 1
-    item.is_sales_item = 1
-    item.is_purchase_item = 1
-
-    item.save()
+# import frappe
+from frappe.tests.utils import FrappeTestCase
 
 
-class TestChannelPartnerPurchaseOrder(unittest.TestCase):
-    def setUp(self):
-        create_item()
-
-    def tearDown(self):
-        frappe.db.rollback()
-
-    def test_create_purchase_order(self):
-        self.assertTrue(True)
+class TestChannelPartnerPurchaseOrder(FrappeTestCase):
+	pass
