6f8ce2ee85d8c11e9ba16e1594d49642250ba45e
Aditya Singh <adityasingh14102001@gmail.com>
2025-04-03 13:48:18 +0530

    fix: add checks in before_install script

--- suntek_app/install/__init__.py
+++ suntek_app/install/__init__.py
@@ -1,4 +1,8 @@
-def before_install():
-    from suntek_app.channel_partner import setup_channel_partner
+import frappe
 
-    setup_channel_partner()
+from suntek_app.channel_partner import setup_channel_partner
+
+
+def before_install():
+    if not frappe.db.exists("Role", "Channel Partner"):
+        setup_channel_partner()
