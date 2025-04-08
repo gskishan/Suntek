8ff6434109c8531846c37f0e5d09c445181fd58e
Aditya Singh <adityasingh14102001@gmail.com>
2025-04-08 13:38:39 +0530

    feat: initialize frappe ui

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <>
+            <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1>Suntek Dashboard</h1>
+            </main>
+        </>
+    );
+};
+
+export default App;
