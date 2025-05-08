2573d285c27e4a75439e90e4d8d5a1897d0a554a
Aditya Singh <adityasingh14102001@gmail.com>
2025-05-08 14:35:26 +0530

    fix: share opportunity with designer

--- suntek_app/overrides/docshare.py
+++ suntek_app/overrides/docshare.py
@@ -10,25 +10,26 @@ def share_opportunity_with_designer(doc, method):
     if doc.share_doctype == "Sales Order":
         sales_order = frappe.get_doc("Sales Order", {"name": doc.share_name})
         opportunity = sales_order.custom_opportunity_name
-        opportunity_doc = frappe.get_doc("Opportunity", opportunity)
+        if opportunity:
+            opportunity_doc = frappe.get_doc("Opportunity", opportunity)
 
-        if not frappe.db.exists(
-            "DocShare",
-            {
-                "share_doctype": "Opportunity",
-                "share_name": opportunity_doc.name,
-                "user": doc.user,
-            },
-        ):
-            opportunity_docshare = frappe.new_doc("DocShare")
+            if not frappe.db.exists(
+                "DocShare",
+                {
+                    "share_doctype": "Opportunity",
+                    "share_name": opportunity_doc.name,
+                    "user": doc.user,
+                },
+            ):
+                opportunity_docshare = frappe.new_doc("DocShare")
 
-            opportunity_docshare.share_doctype = "Opportunity"
-            opportunity_docshare.share_name = opportunity_doc.name
-            opportunity_docshare.user = doc.user
-            opportunity_docshare.read = doc.read
-            opportunity_docshare.write = doc.write
-            opportunity_docshare.share = doc.share
-            opportunity_docshare.submit = 0
-            opportunity_docshare.notify_by_email = 1
+                opportunity_docshare.share_doctype = "Opportunity"
+                opportunity_docshare.share_name = opportunity_doc.name
+                opportunity_docshare.user = doc.user
+                opportunity_docshare.read = doc.read
+                opportunity_docshare.write = doc.write
+                opportunity_docshare.share = doc.share
+                opportunity_docshare.submit = 0
+                opportunity_docshare.notify_by_email = 1
 
-            opportunity_docshare.insert()
+                opportunity_docshare.insert()
